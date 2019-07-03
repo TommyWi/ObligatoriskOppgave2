0d37d81992760437ea83a01cda75eb9c151a60c9
TommyWi <47241834+TommyWi@users.noreply.github.com>
2019-07-03 13:48:48 +0200

    oblig2

---
+++ SimpleChess/SimpleChess/Rook.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleChess
+{
+    public class Rook : Piece
+    {
+        public Rook(string symbol) : base(symbol)
+        {
+
+        }
+
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+        }
+    }
+}
